20e84225dafe8a9c3e5dde0baab8a5664d7b8913
Sparsh Gupta <114053381+gsparsh17@users.noreply.github.com>
2023-12-08 03:24:47 +0530

    Add files via upload

---
+++ game3.js
@@ -0,0 +1,10 @@
+let one=localStorage.one;
+let second=localStorage.second;
+let head=document.getElementById("heading");
+console.log(one);
+if(one==10){
+    head.innerHTML="YOU WIN";
+}
+if(second==10){
+    head.innerHTML="CPU WIN";
+}

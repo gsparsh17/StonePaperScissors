504dca9d1e628a866313310156a3926e1698f271
Sparsh Gupta <114053381+gsparsh17@users.noreply.github.com>
2024-09-06 12:26:25 +0530

    Update game3.js

--- game3.js
+++ game3.js
@@ -8,15 +8,3 @@ if(one==10){
 }
 if(second==10){
     head.innerHTML="CPU WIN";
-=======
-let one=localStorage.one;
-let second=localStorage.second;
-let head=document.getElementById("heading");
-console.log(one);
-if(one==10){
-    head.innerHTML="YOU WIN";
-}
-if(second==10){
-    head.innerHTML="CPU WIN";
->>>>>>> 20e84225dafe8a9c3e5dde0baab8a5664d7b8913
-}
